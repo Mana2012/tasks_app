9e9a69b5ca7cf25041db2b1d52f83ff33e8d6bb4
Manuel Nunez <mana2009@unitec.edu>
2012-05-21 13:00:41 -0600

    creando users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Tasks::Application.routes.draw do
 
+  resources :users
+
 	resources :tasks
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515173038) do
+ActiveRecord::Schema.define(:version => 20120521171450) do
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
     t.integer  "priority"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
 
-#las columnas pueden tener propiedades llamadas constraints.
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "username"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
