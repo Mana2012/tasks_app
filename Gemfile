0c4c60ffde59c7a22d532a41cc2f9aff163c55b9
Manuel Nunez <mana2009@unitec.edu>
2012-05-24 09:14:49 -0600

    autenticacion funcionando

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ gem 'rails', '3.2.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'sqlite3', :group => :development
+gem 'pg', :group => :production
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -38,6 +39,3 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 # gem 'ruby-debug19', :require => 'ruby-debug'
 gem 'therubyracer', group: :development
 gem 'rb-readline'
-gem 'sqlite3'
-gem 'sqlite3', :group => :development
-gem 'pg', :group => :production
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,15 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery   		#se asegura que los form vengan de nuestro form
 	
+	private
 	def current_user
 		if session[:user_id]		#si existe el usuario
-			@current_user ||= User.find[session[:user_id]]	#buscarlo en la clase user
+			@current_user ||= User.find(session[:user_id])	#buscarlo en la clase user
 		end
 	end
 	
-	def logged_in? #si la sesion actual esta abierta
-		current_user present?
+	def logged_in? 			#si la sesion actual esta abierta
+		current_user !=nil
 	end
 	
 	helper_method :current_user, :logged_in?
